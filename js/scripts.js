088157c407f88b0d90f10364d9b68a172469957a
Megan Kaluza <megankaluza@megans-mbp.home>
2019-12-11 19:55:46 -0800

    add new photos/clean up code

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,3 @@
-//Time and Date Stamp
-var MONTH_NAME = ['January', 'Febuary', 'March', 'April', 'May', 'June',
-                  'July', 'August', 'September', 'October', 'November', 'December'];
-function showTime() {
-    function twoDigit(n) {
-        return ('0' + n).slice(-2);
-    }
-    function iso8601(date) {
-        return date.getFullYear() +
-               '-' + twoDigit(1 + date.getMonth()) +
-               '-' + twoDigit(date.getDate()) +
-               'T' + twoDigit(date.getHours()) +
-               ':' + twoDigit(date.getMinutes());
-    }
-    function en_US(date) {
-        var h = date.getHours() % 12;
-        return MONTH_NAME[date.getMonth()] +
-               ' '  + date.getDate() +
-               ', ' + date.getFullYear() +
-               '<br />' + (h == 0 ? 12 : h) +
-               ':'  + twoDigit(date.getMinutes()) +
-               ' ' + (date.getHours() < 12 ? 'am' : 'pm');
-    }
-
-    var timeEl = document.getElementById('time');
-    if (timeEl !== null) {
-        var now = new Date();
-        timeEl.innerHTML = en_US(now);
-        timeDiv.setAttribute('datetime', iso8601(now));
-    }
-};
-setInterval(showTime, 1000);
-
 $(document).ready(function(){
   //Smooth Scrolling
   $('a[href*="#"]:not([href="#"])').click(function() {
@@ -58,57 +25,6 @@ $(document).ready(function(){
   //Exit Modal
   $('.exit').click(function(){
     $('.modal').modal('hide');
-  });
-
-  //Insert New Photos
-  $('#insert1').click(function(){
-    $('.insert-one')
-    .modal({
-      blurring: true
-    })
-    .modal('show');
-  });
-  $('#insert2').click(function(){
-    $('.insert-two')
-    .modal({
-      blurring: true
-    })
-    .modal('show');
-  });
-  $('#insert3').click(function(){
-    $('.insert-three')
-    .modal({
-      blurring: true
-    })
-    .modal('show');
-  });
-  $('#insert4').click(function(){
-    $('.insert-four')
-    .modal({
-      blurring: true
-    })
-    .modal('show');
-  });
-  $('#insert5').click(function(){
-    $('.insert-five')
-    .modal({
-      blurring: true
-    })
-    .modal('show');
-  });
-  $('#insert6').click(function(){
-    $('.insert-six')
-    .modal({
-      blurring: true
-    })
-    .modal('show');
-  });
-  $('#insert7').click(function(){
-    $('.insert-seven')
-    .modal({
-      blurring: true
-    })
-    .modal('show');
   });
 
   //Photos
@@ -231,159 +147,160 @@ $(document).ready(function(){
     })
     .modal('show');
   });
-  $('#open19').click(function(){
-    $('.nineteen')
-    .modal({
-      blurring: true
-    })
-    .modal('show');
-  });
-  $('#open20').click(function(){
-    $('.twenty')
-    .modal({
-      blurring: true
-    })
-    .modal('show');
-  });
-  $('#open21').click(function(){
-    $('.twentyOne')
-    .modal({
-      blurring: true
-    })
-    .modal('show');
-  });
-  $('#open22').click(function(){
-    $('.twentyTwo')
-    .modal({
-      blurring: true
-    })
-    .modal('show');
-  });
-  $('#open23').click(function(){
-    $('.twentyThree')
-    .modal({
-      blurring: true
-    })
-    .modal('show');
-  });
-  $('#open24').click(function(){
-    $('.twentyFour')
-    .modal({
-      blurring: true
-    })
-    .modal('show');
-  });
-  $('#open25').click(function(){
-    $('.twentyFive')
-    .modal({
-      blurring: true
-    })
-    .modal('show');
-  });
-  $('#open26').click(function(){
-    $('.twentySix')
-    .modal({
-      blurring: true
-    })
-    .modal('show');
-  });
-  $('#open27').click(function(){
-    $('.twentySeven')
-    .modal({
-      blurring: true
-    })
-    .modal('show');
-  });
-  $('#open28').click(function(){
-    $('.twentyEight')
-    .modal({
-      blurring: true
-    })
-    .modal('show');
-  });
-  $('#open29').click(function(){
-    $('.twentyNine')
-    .modal({
-      blurring: true
-    })
-    .modal('show');
-  });
-  $('#open30').click(function(){
-    $('.thirty')
-    .modal({
-      blurring: true
-    })
-    .modal('show');
-  });
-  $('#open31').click(function(){
-    $('.thirtyOne')
-    .modal({
-      blurring: true
-    })
-    .modal('show');
-  });
-  $('#open32').click(function(){
-    $('.thirtyTwo')
-    .modal({
-      blurring: true
-    })
-    .modal('show');
-  });
-  $('#open33').click(function(){
-    $('.thirtyThree')
-    .modal({
-      blurring: true
-    })
-    .modal('show');
-  });
-  $('#open34').click(function(){
-    $('.thirtyFour')
-    .modal({
-      blurring: true
-    })
-    .modal('show');
-  });
-  $('#open35').click(function(){
-    $('.thirtyFive')
-    .modal({
-      blurring: true
-    })
-    .modal('show');
-  });
-  $('#open36').click(function(){
-    $('.thirtySix')
-    .modal({
-      blurring: true
-    })
-    .modal('show');
-  });
-  $('#open37').click(function(){
-    $('.thirtySeven')
-    .modal({
-      blurring: true
-    })
-    .modal('show');
-  });
-  $('#open38').click(function(){
-    $('.thirtyEight')
-    .modal({
-      blurring: true
-    })
-    .modal('show');
-  });
-  $('#open39').click(function(){
-    $('.thirtyNine')
-    .modal({
-      blurring: true
-    })
-    .modal('show');
-  });
-  $('#open40').click(function(){
-    $('.forty')
-    .modal({
-      blurring: true
-    })
-    .modal('show');
-  });
+  
+  // $('#open19').click(function(){
+  //   $('.nineteen')
+  //   .modal({
+  //     blurring: true
+  //   })
+  //   .modal('show');
+  // });
+  // $('#open20').click(function(){
+  //   $('.twenty')
+  //   .modal({
+  //     blurring: true
+  //   })
+  //   .modal('show');
+  // });
+  // $('#open21').click(function(){
+  //   $('.twentyOne')
+  //   .modal({
+  //     blurring: true
+  //   })
+  //   .modal('show');
+  // });
+  // $('#open22').click(function(){
+  //   $('.twentyTwo')
+  //   .modal({
+  //     blurring: true
+  //   })
+  //   .modal('show');
+  // });
+  // $('#open23').click(function(){
+  //   $('.twentyThree')
+  //   .modal({
+  //     blurring: true
+  //   })
+  //   .modal('show');
+  // });
+  // $('#open24').click(function(){
+  //   $('.twentyFour')
+  //   .modal({
+  //     blurring: true
+  //   })
+  //   .modal('show');
+  // });
+  // $('#open25').click(function(){
+  //   $('.twentyFive')
+  //   .modal({
+  //     blurring: true
+  //   })
+  //   .modal('show');
+  // });
+  // $('#open26').click(function(){
+  //   $('.twentySix')
+  //   .modal({
+  //     blurring: true
+  //   })
+  //   .modal('show');
+  // });
+  // $('#open27').click(function(){
+  //   $('.twentySeven')
+  //   .modal({
+  //     blurring: true
+  //   })
+  //   .modal('show');
+  // });
+  // $('#open28').click(function(){
+  //   $('.twentyEight')
+  //   .modal({
+  //     blurring: true
+  //   })
+  //   .modal('show');
+  // });
+  // $('#open29').click(function(){
+  //   $('.twentyNine')
+  //   .modal({
+  //     blurring: true
+  //   })
+  //   .modal('show');
+  // });
+  // $('#open30').click(function(){
+  //   $('.thirty')
+  //   .modal({
+  //     blurring: true
+  //   })
+  //   .modal('show');
+  // });
+  // $('#open31').click(function(){
+  //   $('.thirtyOne')
+  //   .modal({
+  //     blurring: true
+  //   })
+  //   .modal('show');
+  // });
+  // $('#open32').click(function(){
+  //   $('.thirtyTwo')
+  //   .modal({
+  //     blurring: true
+  //   })
+  //   .modal('show');
+  // });
+  // $('#open33').click(function(){
+  //   $('.thirtyThree')
+  //   .modal({
+  //     blurring: true
+  //   })
+  //   .modal('show');
+  // });
+  // $('#open34').click(function(){
+  //   $('.thirtyFour')
+  //   .modal({
+  //     blurring: true
+  //   })
+  //   .modal('show');
+  // });
+  // $('#open35').click(function(){
+  //   $('.thirtyFive')
+  //   .modal({
+  //     blurring: true
+  //   })
+  //   .modal('show');
+  // });
+  // $('#open36').click(function(){
+  //   $('.thirtySix')
+  //   .modal({
+  //     blurring: true
+  //   })
+  //   .modal('show');
+  // });
+  // $('#open37').click(function(){
+  //   $('.thirtySeven')
+  //   .modal({
+  //     blurring: true
+  //   })
+  //   .modal('show');
+  // });
+  // $('#open38').click(function(){
+  //   $('.thirtyEight')
+  //   .modal({
+  //     blurring: true
+  //   })
+  //   .modal('show');
+  // });
+  // $('#open39').click(function(){
+  //   $('.thirtyNine')
+  //   .modal({
+  //     blurring: true
+  //   })
+  //   .modal('show');
+  // });
+  // $('#open40').click(function(){
+  //   $('.forty')
+  //   .modal({
+  //     blurring: true
+  //   })
+  //   .modal('show');
+  // });
 
 });
